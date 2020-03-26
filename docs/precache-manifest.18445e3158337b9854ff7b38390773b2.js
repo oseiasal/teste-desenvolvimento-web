9da03967a8b35152e1025261bb55940062c5851b
Oseias Nascimento <39315752+oseiasal@users.noreply.github.com>
2020-03-26 13:13:01 -0300

    atualização do endereço pelo package.json

---
+++ docs/precache-manifest.18445e3158337b9854ff7b38390773b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "178ba33d3b93d0563350c4d80923330c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "696cb7fd619685d17230",
+    "url": "./static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "696cb7fd619685d17230",
+    "url": "./static/js/2.df48196d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.df48196d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a67962dbf35599ba05d",
+    "url": "./static/js/main.a5bedc3f.chunk.js"
+  },
+  {
+    "revision": "2e336bd63886205bb375",
+    "url": "./static/js/runtime-main.f6e3774b.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/teste-desenvolvimento-web/precache-manifest.1d9b8044325a5670ca64868746fbba36.js"
+  "./precache-manifest.18445e3158337b9854ff7b38390773b2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/teste-desenvolvimento-web/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
